867e7150ce0f76104a4bfcace57d86c9be6f00aa
Stefan Oprea <soprea@wayfair.com>
2018-09-06 15:28:42 +0200

    Http enhanced backend

--- backend/remote-state/http/backend.go
+++ backend/remote-state/http/backend.go
@@ -6,6 +6,7 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -75,13 +76,11 @@ func New() backend.Backend {
 				Default:     false,
 				Description: "(Optional) Whether to skip TLS verification. Defaults to false.",
 			},
-
-			"local_cert_ca_file": {
+			"local_ca_file": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Description: "CA to use when the rest api is using a self signed certificate.",
+				Description: "CA to be used for TLS",
 			},
-
 			"mutual_tls_authentication": {
 				Type:        schema.TypeBool,
 				Optional:    true,
@@ -90,24 +89,15 @@ func New() backend.Backend {
 			},
 
 			"local_cert_file": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				ConflictsWith: []string{"local_cert_ca_file"},
-				Description:   "Cert file to be used for mutual tls authentication.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Cert file to be used for mutual tls authentication.",
 			},
 
 			"local_key_file": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				ConflictsWith: []string{"local_cert_ca_file"},
-				Description:   "Key file to be used for mutual tls authentication.",
-			},
-
-			"local_ca_file": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				ConflictsWith: []string{"local_cert_ca_file"},
-				Description:   "CA to be used for mutual tls authentication.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Key file to be used for mutual tls authentication.",
 			},
 		},
 	}
@@ -199,22 +189,22 @@ func (b *Backend) configure(ctx context.Context) error {
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
-
+	// skip_cert_verification client
 	if v, ok := data.GetOk("skip_cert_verification"); ok {
-		b.skipTLS = v.(bool)
-		if b.skipTLS {
+		if b.skipTLS = v.(bool); b.skipTLS {
 			// If skip_cert_verification = true, the address must be of type HTTPS
 			if !isHTTPS(addressURL) {
 				return fmt.Errorf("Address must be of type HTTPS if skip_cert_verification = true")
 			}
-			// If local_cert_ca_file is also set, raise an error
-			if v, ok := data.GetOk("local_cert_ca_file"); ok {
-				return fmt.Errorf("skip_cert_verification is %t and local_cert_ca_file is set: %s. please choose one or the other", b.skipTLS, v)
+			// If local_ca_file is also set, raise an error
+			if v, ok := data.GetOk("local_ca_file"); ok {
+				return fmt.Errorf("skip_cert_verification is %t and local_ca_file is set: %s. please choose one or the other", b.skipTLS, v)
 			}
 			// If mutual_tls_authentication is also set, raise an error
 			if data.Get("mutual_tls_authentication").(bool) == true {
 				return fmt.Errorf("skip_cert_verification is true and mutual_tls_authentication is set. please choose one or the other")
 			}
+			log.Printf("[DEBUG] Using https client with skipping cert verification")
 			// add the option to ignores TLS verification to our client
 			client.Transport = &http.Transport{
 				TLSClientConfig: &tls.Config{
@@ -223,14 +213,14 @@ func (b *Backend) configure(ctx context.Context) error {
 			}
 		}
 	}
-
-	if v, ok := data.GetOk("local_cert_ca_file"); ok {
+	// CA provided client and mutual tls authentication client
+	if v, ok := data.GetOk("local_ca_file"); ok {
 		// If local_cert_ca_file exists, the address must be of type HTTPS
 		if !isHTTPS(addressURL) {
-			return fmt.Errorf("Address must be of type HTTPS if local_cert_ca_file is set")
+			return fmt.Errorf("Address must be of type HTTPS if local_ca_file is set")
 		}
 		if data.Get("skip_cert_verification").(bool) == true {
-			return fmt.Errorf("skip_cert_verification is true and local_cert_ca_file is set. Please choose one or the other")
+			return fmt.Errorf("skip_cert_verification is true and local_ca_file is set. Please choose one or the other")
 		}
 		b.localCertCAFile = v.(string)
 
@@ -249,66 +239,42 @@ func (b *Backend) configure(ctx context.Context) error {
 			return fmt.Errorf("No certs could be appended: %s", cert)
 		}
 		// add RootCas to our client
+		log.Printf("[DEBUG] Using https client with provided CA")
 		client.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{
 				RootCAs: rootCAs,
 			},
 		}
-	}
-
-	if v, ok := data.GetOk("mutual_tls_authentication"); ok {
-		if b.mutualTLS = v.(bool); b.mutualTLS {
-			// If mutual_tls_authentication = true, the address must be of type HTTPS
-			if !isHTTPS(addressURL) {
-				return fmt.Errorf("Address must be of type HTTPS if mutual_tls_authentication = true")
-			}
-			// If mutual_tls_authentication = true, the local_cert_file needs to be set.
-			if v, ok := data.GetOk("local_cert_file"); ok {
-				b.localCertFile = v.(string)
-			} else {
-				return fmt.Errorf("mutual_tls_authentication is true and local_cert_file is not set %s", b.localCertFile)
-			}
-			// If mutual_tls_authentication = true, the local_key_file needs to be set.
-			if v, ok := data.GetOk("local_key_file"); ok {
-				b.localKeyFile = v.(string)
-			} else {
-				return fmt.Errorf("mutual_tls_authentication is true and local_key_file is not set %s", b.localKeyFile)
-			}
-			// If mutual_tls_authentication = true, the local_ca_file needs to be set.
-			if v, ok := data.GetOk("local_ca_file"); ok {
-				b.localCAFile = v.(string)
-			} else {
-				return fmt.Errorf("mutual_tls_authentication is true and local_ca_file is not set %s", b.localCAFile)
-			}
-			// load client cert
-			certs, err := tls.LoadX509KeyPair(b.localCertFile, b.localKeyFile)
-			if err != nil {
-				return fmt.Errorf("Can not load pem files: %s and : %s. Error: %s", b.localCertFile, b.localKeyFile, err)
-			}
-			// Get the SystemCertPool, continue with an empty pool on error
-			rootCAs, _ := x509.SystemCertPool()
-			if rootCAs == nil {
-				rootCAs = x509.NewCertPool()
-			}
-			// Read in the ca cert file
-			cert, err := ioutil.ReadFile(b.localCAFile)
-			if err != nil {
-				return fmt.Errorf("Failed to read %s into memory: %s", b.localCAFile, err)
-			}
-			// Append our cert to the system pool
-			if ok := rootCAs.AppendCertsFromPEM(cert); !ok {
-				return fmt.Errorf("No certs could be appended: %s", cert)
-			}
-			// add the certificate and the rootCAs for our mutual tls authentication
-			client.Transport = &http.Transport{
-				TLSClientConfig: &tls.Config{
-					Certificates: []tls.Certificate{certs},
-					RootCAs:      rootCAs,
-				},
+		if v, ok := data.GetOk("mutual_tls_authentication"); ok {
+			if b.mutualTLS = v.(bool); b.mutualTLS {
+				// If mutual_tls_authentication = true, the local_key_file needs to be set.
+				if v, ok := data.GetOk("local_key_file"); ok {
+					b.localKeyFile = v.(string)
+				} else {
+					return fmt.Errorf("mutual_tls_authentication is true and local_key_file is not set %s", b.localKeyFile)
+				}
+				// If mutual_tls_authentication = true, the local_cert_file needs to be set.
+				if v, ok := data.GetOk("local_cert_file"); ok {
+					b.localCertFile = v.(string)
+				} else {
+					return fmt.Errorf("mutual_tls_authentication is true and local_cert_file is not set %s", b.localCertFile)
+				}
+				// load client cert
+				certs, err := tls.LoadX509KeyPair(b.localCertFile, b.localKeyFile)
+				if err != nil {
+					return fmt.Errorf("Can not load pem files: %s and : %s. Error: %s", b.localCertFile, b.localKeyFile, err)
+				}
+				log.Printf("[DEBUG] Using https client with provided CA and mutual tls authentication")
+				// add the certificate for our mutual tls authentication
+				client.Transport = &http.Transport{
+					TLSClientConfig: &tls.Config{
+						Certificates: []tls.Certificate{certs},
+						RootCAs:      rootCAs,
+					},
+				}
 			}
 		}
 	}
-
 	b.client = client
 	return nil
 }
